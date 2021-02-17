b2cb0d29e33fa70e5d62aa9a9b339dee18b46a21
julieconnors <juliana.g.connors@gmail.com>
2021-02-17 15:08:07 -0500

    add index action with serializer

--- backend/app/controllers/api/v1/artists_controller.rb
+++ backend/app/controllers/api/v1/artists_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ArtistsController < ApplicationController
 
     def index
-        
+        artists = Artist.all
+        render json: ArtistSerializer.new(artists)
     end
 end
--- backend/app/controllers/api/v1/artworks_controller.rb
+++ backend/app/controllers/api/v1/artworks_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ArtworksController < ApplicationController
 
     def index
-        
+        artworks = Artwork.all
+        render json: ArtworkSerializer.new(artworks)
     end
 end
