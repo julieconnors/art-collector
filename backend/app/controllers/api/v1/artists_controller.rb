79a2004588cc611bf100384322046d9fbcd12165
julieconnors <juliana.g.connors@gmail.com>
2021-02-17 14:49:14 -0500

    create namespaced controllers

---
+++ backend/app/controllers/api/v1/artists_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ArtistsController < ApplicationController
+
+    def index
+        
+    end
+end
---
+++ backend/app/controllers/api/v1/artworks_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::ArtworksController < ApplicationController
+
+    def index
+        
+    end
+end
