9eed7b10cd75889fc2fba9ecb384d3d60ae6a4ba
julieconnors <juliana.g.connors@gmail.com>
2021-02-17 20:27:16 -0500

    add create action and strong params

--- backend/app/controllers/api/v1/artworks_controller.rb
+++ backend/app/controllers/api/v1/artworks_controller.rb
@@ -4,4 +4,21 @@ class Api::V1::ArtworksController < ApplicationController
         artworks = Artwork.all
         render json: ArtworkSerializer.new(artworks)
     end
+
+    def create
+        # binding.pry
+        artwork = Artwork.new(artwork_params)
+
+        if artwork.save
+            render json: ArtworkSerializer.new(artwork)
+        else
+            render json: { errors: artwork.errors.full_messages }
+        end
+    end
+
+    private
+
+    def artwork_params
+        params.require(:artwork).permit(:title, :image, :artist_id)
+    end
 end
