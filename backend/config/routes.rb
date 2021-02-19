16aa9717eee54f4ca590b7da174ecc13f3079bd7
julieconnors <juliana.g.connors@gmail.com>
2021-02-18 20:41:25 -0500

    add show and destroy route to artwork model

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :artworks, only: [:index, :create]
+      resources :artworks, only: [:index, :create, :show, :destroy]
       resources :artists, only: [:index]
     end
   end
