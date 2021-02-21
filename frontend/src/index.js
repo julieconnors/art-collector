1205a1238ebf898b4f2ec8da503b48306ad16f7f
julieconnors <juliana.g.connors@gmail.com>
2021-02-21 17:01:13 -0500

    create service class instance and move fetch requests into service class

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,28 +1,13 @@
-const artworkEndpoint = "http://localhost:3000/api/v1/artworks"
+const apiService = new Service()
 
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector("button")
+    button.addEventListener("click", () => displayForm())
 
-    fetchArtworks()
-
-    button.addEventListener("click", () => renderForm())
+    apiService.fetchArtworks()
 })
 
-function fetchArtworks() {
-    fetch(artworkEndpoint)
-    .then(res => res.json())
-    .then(json => json.data.forEach(obj => {
-        let artwork = new Artwork(obj, obj.attributes)
-        artwork.renderArtwork()
-        
-        document.querySelector(`.delete-${artwork.id}`).addEventListener("click", () => {
-            deleteFetch(artwork.id)
-        })
-    }
-    ))
-}
-
-function renderForm(){
+function displayForm(){
     document.querySelector("#form-container").style.visibility = "visible"
     document.querySelector("button").style.visibility = 'hidden'
 
@@ -41,42 +26,5 @@ function createArtworkFormHandler(e){
     const artworkImage = document.querySelector("#artwork-image").value
     const artistId = parseInt(document.querySelector("#artwork-artist").value)
 
-    postFetch(artworkTitle, artworkImage, artistId)
-}
-
-function postFetch(title, image, artist_id) {
-    const artworkData = {title, image, artist_id}
-    fetch(artworkEndpoint, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Accept": "application/json"
-        },
-        body: JSON.stringify(artworkData)
-    })
-    .then(res => res.json())
-    .then(artwork => {
-        let newArtwork = new Artwork(artwork.data, artwork.data.attributes)
-        newArtwork.renderArtwork()
-
-        document.querySelector(`.delete-${newArtwork.id}`).addEventListener("click", () => {
-            deleteFetch(newArtwork.id)
-        })
-
-        document.querySelector("#form-container").style.visibility = "hidden"
-        document.querySelector("button").style.visibility = "visible"
-    })
-}
-
-function deleteFetch(id) {
-    fetch(`http://localhost:3000/api/v1/artworks/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
-    .then(resp => resp.json())
-    .then(artwork => {
-        document.getElementById(`${artwork.data.id}`).remove()
-    })
+    apiService.postFetch(artworkTitle, artworkImage, artistId)
 }
