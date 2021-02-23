51357044b572b027be88b24014eb22a9be74362e
julieconnors <juliana.g.connors@gmail.com>
2021-02-23 06:28:56 -0500

    remove artwork button listener and move form listener to DOM content load

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,34 +2,23 @@ const apiService = new Service()
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    const addArtworkButton = document.querySelector("#add-artwork-button")
+    // const addArtworkButton = document.querySelector("#add-artwork-button")
     const addArtistButton = document.querySelector("#add-artist-button")
     
-    addArtistButton.addEventListener("click", () => createArtistForm())
-    addArtworkButton.addEventListener("click", () => displayForm())
+    addArtistButton.addEventListener("click", () => renderArtistForm())
+    // addArtworkButton.addEventListener("click", () => displayForm())
 
+    mountFormListener()
     apiService.fetchArtworks()
     apiService.fetchArtists()
 })
 
-function createArtistForm() {
-    console.log("ADD ARtists")
-    const html = 
-        `<p>Add Artist</p>
-        <form class="artist-form">
-            <input type="text" name="artist-name" placeholder="Artist Name" id="artist-name">
-            <br>
-            <input type="submit" value="Submit" class="submit" id="submit-artist">
-        </form>`
-
-    const formDiv = document.querySelector(".form-container")
-    formDiv.innerHTML += html
-
-    document.querySelector(".add-artist").style.visibility = "hidden"
-    document.querySelector(".artwork-form").style.visibility = "hidden"
-
+function renderArtistForm() {
     const addArtistForm = document.querySelector(".artist-form")
+    addArtistForm.style.visibility = "visible"
     addArtistForm.addEventListener("submit", (e) => artistFormHandler(e))
+
+    document.querySelector(".artist-button-div").style.visibility = "hidden"
 }
 
 function artistFormHandler(e) {
