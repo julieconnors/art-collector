09921e7ce8b6305ef949fc5d10cd549436bc9c62
julieconnors <juliana.g.connors@gmail.com>
2021-02-22 20:49:48 -0500

    add function to render new artist form

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,48 @@
 const apiService = new Service()
 
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.querySelector("button")
-    button.addEventListener("click", () => displayForm())
+
+    const addArtworkButton = document.querySelector("#add-artwork-button")
+    const addArtistButton = document.querySelector("#add-artist-button")
+    
+    addArtistButton.addEventListener("click", () => createArtistForm())
+    addArtworkButton.addEventListener("click", () => displayForm())
 
     apiService.fetchArtworks()
+    apiService.fetchArtists()
 })
 
-function displayForm(){
-    document.querySelector("#form-container").style.visibility = "visible"
-    document.querySelector("button").style.visibility = 'hidden'
+function createArtistForm() {
+    console.log("ADD ARtists")
+    const html = 
+        `<p>Add Artist</p>
+        <form class="artist-form">
+            <input type="text" name="artist-name" placeholder="Artist Name" id="artist-name">
+            <br>
+            <input type="submit" value="Submit" class="submit" id="submit-artist">
+        </form>`
+
+    const formDiv = document.querySelector(".form-container")
+    formDiv.innerHTML += html
+
+    document.querySelector(".add-artist").style.visibility = "hidden"
+    document.querySelector(".artwork-form").style.visibility = "hidden"
+
+    const addArtistForm = document.querySelector(".artist-form")
+    addArtistForm.addEventListener("submit", (e) => artistFormHandler(e))
+}
+
+function artistFormHandler(e) {
+    e.preventDefault()
+
+    artistName = document.querySelector("#artist-name").value
+
+    apiService.postArtistFetch(artistName)
+}
+
+function displayForm() {
+    document.querySelector(".form-container").style.visibility = "visible"
+    document.querySelector("#add-artwork-button").style.visibility = 'hidden'
 
     mountFormListener()
 }
@@ -19,7 +52,7 @@ function mountFormListener() {
     form.addEventListener("submit", (e) => createArtworkFormHandler(e))
 }
 
-function createArtworkFormHandler(e){
+function createArtworkFormHandler(e) {
     e.preventDefault()
 
     const artworkTitle = document.querySelector("#artwork-title").value
